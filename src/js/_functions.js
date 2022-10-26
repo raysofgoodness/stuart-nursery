1c5f38d5782068a8538aa41967e1496928478869
raysofgoodness <shady5324@gmail.com>
2022-10-26 17:00:58 +0300

    services section styling

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -1,8 +1,5 @@
 import  {burgerTrigger}  from './functions/burger';
 import  {animationHeroBlock} from "./functions/animations";
-import AOS from 'aos';
-AOS.init();
-
 
 burgerTrigger();
 animationHeroBlock();
