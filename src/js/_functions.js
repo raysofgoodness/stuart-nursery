96744b3a351fe4d7594cb806ee3bd8877ef737cb
raysofgoodness <shady5324@gmail.com>
2022-10-26 15:57:30 +0300

    init service section

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -1,7 +1,11 @@
 import  {burgerTrigger}  from './functions/burger';
 import  {animationHeroBlock} from "./functions/animations";
+import AOS from 'aos';
+AOS.init();
+
 
 burgerTrigger();
 animationHeroBlock();
+
 
 
