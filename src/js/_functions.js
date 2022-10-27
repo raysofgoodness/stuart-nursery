6963f19dcc1f9a24f2171e83cec94ff02fb4f479
raysofgoodness <shady5324@gmail.com>
2022-10-27 16:39:28 +0300

    active slider

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -4,5 +4,28 @@ import  {animationHeroBlock} from "./functions/animations";
 burgerTrigger();
 animationHeroBlock();
 
+import Swiper, {Navigation} from 'swiper';
+Swiper.use([Navigation]);
+
+const swiper = new Swiper(`.favorites-slider`, {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  }
+});
+
+
 
 
